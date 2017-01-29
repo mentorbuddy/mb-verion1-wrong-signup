57deadb6ce42bf247cd3cf85c8c1e7477f498b45
Viresh Dhawan <mentor2buddy@gmail.com>
2017-01-29 22:20:47 +0530

    Updated

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,6 +1,6 @@
 class UniversitiesController < ApplicationController
 	before_action :find_university, only: [:show, :dashboard]
-	before_action :find_episode, only: [:show, :dashboard]
+	before_action :find_mentor, only: [:show, :dashboard]
 
 	def index
 		@universities = University.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
@@ -14,8 +14,8 @@ class UniversitiesController < ApplicationController
 
 	private
 
-	def find_episode
-		@episodes = Episode.where(university_id: @university).order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+	def find_mentor
+		@mentors = Mentor.where(university_id: @university).order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def find_university
--- app/models/university.rb
+++ app/models/university.rb
@@ -5,6 +5,6 @@ class University < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :mentors
 
-  has_attached_file :thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }
+  has_attached_file :thumbnail, :styles => { }#:large => "1000x1000#", :medium => "550x550#" }
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
 end
